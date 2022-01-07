efbf6d7aa322f7cc5ffe1b82daf67379fc31dfd5
Alexander Gassmann <agassman@microsoft.com>
2022-01-07 08:50:03 +0000

    Implemented local package generation

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 A dependency package represents a desired state of a system, which is defined by a list of Debian packages with corresponding versions. All required packages are captured as dependencies in a new Debian package.
 
 This repository contains pipelines for generating the Debian dependency package.
+
+# Structure of dependency packages
+Dependency packages consist of a `control` file defining the desired state.
+```
+Package: dependency-pack
+Version: 0.0.1
+Architecture: amd64
+Maintainer: YourName <YourName@YourCompany>
+Depends: dependency-a (=1.2.3-1), dependency-b(=1.9.1-3)
+Description: Dependency Pack.
+ You can add a longer description here. Mind the space at the beginning of this paragraph.
+`
--- src/build-package.sh
+++ src/build-package.sh
@@ -2,17 +2,30 @@
 
 set -euxo pipefail
 
-if [ "$#" -ne 1 ]
+if [ "$#" -ne 2 ]
 then
-	echo "Script requires 1 parameter: 'path to package folder'"
+	echo "Script requires 2 parameters: 'path to package folder', 'path to output folder'"
 	exit 1
 fi
 
-cd $1
-origin=$(ls | head -n 1)
-current=$(ls | tail -n 1)
-added=$(comm -13 $origin $current)
-echo "Added [$added]"
+packageFolder=$(cd $1 && pwd)
+outputFolder=$(mkdir -p $2 && cd $2 && pwd)
 
-removed=$(comm -23 $origin $current)
-echo "Removed [$removed]"
+cd $packageFolder
+origin=$(ls --sort=version | head -n 1)
+current=$(ls --sort=version | tail -n 1)
+added=$(comm -13 $origin $current | tr '\n' ', ')
+echo "Added $added"
+
+removed=$(comm -23 $origin $current | tr '\n' ', ')
+echo "Removed $removed"
+
+mkdir -p $outputFolder/dependency-pack/DEBIAN
+cd $outputFolder/dependency-pack/DEBIAN
+
+echo "Package: dependency-pack" > control
+echo "Version: 0.0.1-$current" >> control
+echo "Architecture: amd64" >> control
+echo "Maintainer: YourName <YourName@YourCompany>" >> control
+echo "Depends: $([ -z "$added" ] && echo $added || echo ${added::-1})" >> control
+echo "Description: Dependency Pack." >> control
